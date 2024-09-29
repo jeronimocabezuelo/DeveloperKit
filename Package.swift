6df2ed17b31c9c0287c85ace6bd43c7950f2889a
Jeronimo Cabezuelo <jeronimocabezuelo@gmail.com>
2024-09-29 15:10:27 +0200

    [WIP] - Se añade SwiftLint al paquete

--- Package.swift
+++ Package.swift
@@ -12,11 +12,15 @@ let package = Package(
             name: "DeveloperKit",
             targets: ["DeveloperKit"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.57.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DeveloperKit"),
-
+            name: "DeveloperKit",
+            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")]
+        ),
     ]
 )
