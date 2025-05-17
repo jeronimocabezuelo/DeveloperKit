8b9ddf64a76ce0045ad2ebfef951e8df973804e7
Jeronimo Cabezuelo <jeronimocabezuelo@gmail.com>
2025-05-17 14:15:45 +0200

    [WIP] - Se añade la extensión de View con un método para elegir el estilo borderedProminent de un botón dependiendo de si es Mac o iOS

---
+++ Sources/DeveloperKit/Extensions/View+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  View+Extension.swift
+//  DeveloperKit
+//
+//  Created by Jerónimo Cabezuelo Ruiz on 17/5/25.
+//
+
+import SwiftUI
+
+extension View {
+    func customBorderedStyle() -> some View {
+#if os(macOS)
+        self.buttonStyle(.bordered)
+#else
+        self.buttonStyle(.borderedProminent)
+#endif
+    }
+}
